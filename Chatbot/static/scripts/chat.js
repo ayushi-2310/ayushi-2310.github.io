05d65b4674ea6dca11e3fcc9809a80aabe2e7a44
AYUSHI GUPTA <65401333+ayushi-2310@users.noreply.github.com>
2023-05-25 09:49:39 +0530

    Update chat.js

--- Chatbot/static/scripts/chat.js
+++ Chatbot/static/scripts/chat.js
@@ -60,7 +60,7 @@ function getResponse() {
     let userText = $("#textInput").val();
 
     if (userText == "") {
-        userText = "I love Code Palace!";
+        userText = "I am here for you!";
     }
 
     let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
@@ -94,7 +94,7 @@ function sendButton() {
 }
 
 function heartButton() {
-    buttonSendText("Heart clicked!")
+    buttonSendText("I love you!")
 }
 
 // Press enter to send a message
